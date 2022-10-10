ba3de38ca40b7d4e7dea6ba017f7aba5b2d7483b
James Serengia <jamesserengia@gmail.com>
2022-10-10 16:56:09 +0300

    Update menu button markup respective css and javascript

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const btnOpen = document.querySelector(".btn-open");
 const btnClose = document.querySelector(".btn-close");
+const navContainer = document.querySelector("item-nav");
 
 btnOpen.addEventListener("click", () => {
-  btnOpen.classList.toggle("visible");
-  btnClose.classList.toggle("visible");
+  btnClose.classList.remove("hidden");
+  //   btnClose.classList.add("visible");
+  btnOpen.classList.add("hidden");
 });
 
 btnClose.addEventListener("click", () => {
-  btnOpen.classList.toggle("visible");
-  btnClose.classList.toggle("visible");
+  btnOpen.classList.remove("hidden");
+  btnClose.classList.add("hidden");
 });
